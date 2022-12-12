ecebb292598b6c932d2365a0732c2868eedafff2
Debaranjan <46136637+debaranjan21@users.noreply.github.com>
2022-12-12 15:22:21 +0530

    Update IndexController.java

--- src/main/java/com/edw/controller/IndexController.java
+++ src/main/java/com/edw/controller/IndexController.java
@@ -17,7 +17,8 @@ public class IndexController {
 
          return new HashMap<>() {{
             put("success", true);
-            put("Hello Debaranjan", "Order-DRE" , "\n");
+            put("Hello Debaranjan", "Order-DRE");
+            put("." ,"\n")
         }};
     }
 }
