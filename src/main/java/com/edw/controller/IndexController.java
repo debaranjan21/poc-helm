fe1e706c5c8d38d8c1dafad5eb5a05896e969a87
Debaranjan <46136637+debaranjan21@users.noreply.github.com>
2022-12-12 15:09:56 +0530

    Update IndexController.java

--- src/main/java/com/edw/controller/IndexController.java
+++ src/main/java/com/edw/controller/IndexController.java
@@ -17,7 +17,7 @@ public class IndexController {
 
          return new HashMap<>() {{
             put("success", true);
-            put("hello", "DREs");
+            put("hello", "Order-DREs");
             put("new-message", "adding a new msg");
         }};
     }
